346693ad13170732768aad7e3b37e003ed992c26
Guilherme Ottoni Escarelli De Alencar <guiottonie@hotmail.com>
2017-10-04 03:17:45 -0300

    Add processors

---
+++ Edp.Api.Command/Processors/FeedbackProcessor.cs
@@ -0,0 +1,14 @@
+﻿using EDP.Api.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edp.Api.Command.Processors
+{
+    public class FeedbackProcessor : Processor
+    {
+        public FeedbackProcessor(String connection, MyOptions _myOptions) : base(connection, _myOptions)
+        {
+        }
+    }
+}
